56cdf804289849794abdabd4d33e09f542076389
Nazar0360 <83352561+Nazar0360@users.noreply.github.com>
2023-09-01 16:25:50 +0300

    Fix collision system, add trails, implement method to spawn bodies on a circle, and refactor code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import Any
 import pygame as pg
 import numpy as np
 import random
@@ -30,31 +29,29 @@ def hsv_to_rgb(h, s, v):
 
     return int(r * 255), int(g * 255), int(b * 255)
 
-def is_rectangle_on_screen(pos1, pos2, screen_width, screen_height):
+def is_rectangle_on_screen(pos1, pos2, surface_size):
     x1, y1 = pos1
     x2, y2 = pos2
 
-    if x1 > screen_width and x2 > screen_width:
+    if x1 > surface_size[0] and x2 > screen_size[0]:
         return False
     if x1 < 0 and x2 < 0:
         return False
-    if y1 > screen_height and y2 > screen_height:
+    if y1 > surface_size[1] and y2 > screen_size[1]:
         return False
     if y1 < 0 and y2 < 0:
         return False
     return True
 
 class Game:
-    def __init__(self, screen_width=800, screen_height=600, fps=60, speed=1):
+    def __init__(self, screen_size=(800, 600), fps=60, speed=1):
         pg.init()
-        self.width = screen_width
-        self.height = screen_height
-        self.screen = pg.display.set_mode((self.width, self.height))
+        self.screen = pg.display.set_mode(screen_size)
         self.clock = pg.time.Clock()
         self.fps = fps
         self.speed = speed
         
-        self.space_system = Game.SpaceSystem()
+        self.space_system = Game.SpaceSystem(screen_size)
         self.body_to_launch = None
 
     def run(self):
@@ -85,7 +82,7 @@ class Game:
     def handle_mouse_button_up(self):
         if self.body_to_launch:
             self.body_to_launch.velocity = (self.body_to_launch.position - pg.mouse.get_pos()) * 0.1
-            self.space_system.add_body(self.body_to_launch)
+            self.space_system.add(self.body_to_launch)
             self.body_to_launch = None
 
     def handle_mousewheel(self, y):
@@ -97,12 +94,11 @@ class Game:
 
     def update_screen(self):
         self.screen.fill((0, 0, 0))
+        self.space_system.draw(self.screen)
         if self.body_to_launch:
             pg.draw.line(self.screen, (255, 255, 255), self.body_to_launch.position, self.body_to_launch.position * 2 - pg.mouse.get_pos())
-        for body in self.space_system.bodies:
-            body.draw(self.screen, self.width, self.height)
         if self.body_to_launch:
-            self.body_to_launch.draw(self.screen, self.width, self.height)
+            self.body_to_launch.draw(self.screen)
 
         pg.display.flip()
 
@@ -116,6 +112,8 @@ class Game:
                 radius = (mass * 3 / (density * 4 * np.pi)) ** (1/3)
             elif radius is not None and density is None and mass is not None:
                 density = mass / ((4 / 3) * np.pi * radius ** 3)
+            elif radius is not None and density is not None and mass is not None:
+                raise ValueError("Invalid combination of parameters: radius, density, and mass cannot all be provided simultaneously")
             else:
                 radius = 10 if radius is None else radius
                 density = 1 if density is None else density
@@ -123,9 +121,12 @@ class Game:
             self.density = float(density)
             self.velocity = np.array(velocity, dtype=np.float64)
             self.position = np.array(position, dtype=np.float64)
+            self.recoil = np.array([0, 0], dtype=np.float64)
 
         def move(self, dt: float):
             self.position += self.velocity * dt * 60
+            self.position -= self.recoil
+            self.recoil *= 0
 
         def apply_gravity_acceleration(self, space_system, dt):
             if self.mass == 0:
@@ -138,44 +139,25 @@ class Game:
                 direction_vector = (body.position - self.position) / distance
                 force = 0.1 * self.mass * body.mass / distance ** 2
                 acceleration_vector = direction_vector * force / self.mass * dt * 60
-                collision_handling_system = 2
                 if distance < (body.radius + self.radius):
                     overlap = self.radius + body.radius - distance
                     total_mass = self.mass + body.mass
                     mass_proportion = self.mass / total_mass
                     distance = np.linalg.norm(self.position - body.position)
-                    if collision_handling_system == 0:
-                        continue
-                    elif collision_handling_system == 1: # FIXME
-                        self.position -= direction_vector * overlap * (1 - mass_proportion)
-                        self.velocity -= acceleration_vector * (1 - mass_proportion)
-                        body.position += direction_vector * overlap * mass_proportion
-                        body.velocity += acceleration_vector * mass_proportion
-
-                        direction_vector = (body.position - self.position) / distance
-                        force = 0.1 * self.mass * body.mass / distance ** 2
-                        acceleration_vector = direction_vector * force / self.mass * dt * 60
-
-                        body.velocity += acceleration_vector
-                        continue
-                    elif collision_handling_system == 2:
-                        space_system.remove_body(self, body)
-                        new_color = mass_proportion * np.array(self.color) + (1 - mass_proportion) * np.array(body.color)
-                        new_color = tuple(new_color.astype(int))
-                        new_density = mass_proportion * self.density + (1 - mass_proportion) * body.density
-                        new_position = self.position + direction_vector * distance / 2 * (1 - mass_proportion)
-                        new_body = Game.Body(color=new_color, density=new_density, mass=total_mass, position=new_position)
-                        space_system.add_body(new_body)
-                        del self, body
-                        return
+                    self.recoil = direction_vector * overlap * (1 - mass_proportion)
+                    self.velocity -= acceleration_vector * (1 - mass_proportion)
+                    body.recoil = -direction_vector * overlap * mass_proportion
+                    body.velocity += acceleration_vector * mass_proportion
+                    continue
                 self.velocity += acceleration_vector
 
-        def draw(self, surface, screen_width, screen_height):
+        def draw(self, surface: pg.SurfaceType):
+            surface_size = surface.get_size()
             pos1 = self.position - (self.radius, self.radius)
             pos2 = self.position + (self.radius, self.radius)
-            if is_rectangle_on_screen(pos1, pos2, screen_width, screen_height):
+            if is_rectangle_on_screen(pos1, pos2, surface_size):
                 pg.draw.circle(surface, self.color, self.position, self.radius)
-        
+
         @property
         def mass(self):
             return self.density * (4 / 3) * np.pi * self.radius ** 3
@@ -186,28 +168,66 @@ class Game:
             self.radius = (mass * 3 / (self.density * 4 * np.pi)) ** (1/3)
     
     class SpaceSystem:
-        def __init__(self, bodies=None):
+        def __init__(self, screen_size=None, *bodies):
             self.bodies = set()
             if bodies:
                 self.bodies |= set(bodies)
+            
+            if screen_size is None:
+                screen_size = pg.display.get_window_size()
+            self.surface = pg.Surface(screen_size, flags=pg.SRCALPHA)
         
         def tick(self, dt):
-            for body in self.bodies.copy():
+            bodies = self.bodies.copy()
+            for body in bodies:
                 body: Game.Body
                 body.apply_gravity_acceleration(self, dt)
+            for body in bodies:
+                body: Game.Body
                 body.move(dt)
         
-        def add_body(self, *bodies):
+        def draw(self, surface: pg.SurfaceType):
+            overlay_surface = pg.Surface(self.surface.get_size(), pg.SRCALPHA)
+            overlay_surface.fill((0,0,0,15))
+            self.surface.blit(overlay_surface, (0,0))
+            for body in self.bodies:
+                body: Game.Body
+                body.draw(self.surface)
+            surface.blit(self.surface, (0, 0))
+
+        def add(self, *bodies):
             for body in bodies:
                 self.bodies.add(body)
         
-        def remove_body(self, *bodies):
+        def remove(self, *bodies):
             for body in bodies:
                 self.bodies -= {body}
+        
+        def spawn_bodies_on_circle(self, num_bodies, center, radius, velocity_magnitude):
+            bodies = []
+            angle_increment = 360 / num_bodies
+
+            for i in range(num_bodies):
+                angle = i * angle_increment
+                hue = angle % 360
+                x = center[0] + radius * np.cos(np.radians(angle))
+                y = center[1] + radius * np.sin(np.radians(angle))
+                velocity_x = -velocity_magnitude * np.sin(np.radians(angle))
+                velocity_y = velocity_magnitude * np.cos(np.radians(angle))
+                color = hsv_to_rgb(hue, 1, 1)
+                body = Game.Body(position=(x, y), velocity=(velocity_x, velocity_y), color=color)
+                bodies.append(body)
+            
+            self.add(*bodies)
+        
+        def resize_surface(self, new_size):
+            new_surface = pg.Surface(new_size, flags=pg.SRCALPHA)
+            new_surface.blit(self.surface, (0, 0))
+            self.surface = new_surface
 
 
 if __name__ == "__main__":
-    game = Game(speed=1)
-    game.space_system.add_body(Game.Body(color=(255, 0, 0), position=(100, 100), radius=20))
-    game.space_system.add_body(Game.Body(color=(0, 0, 255), position=(500, 500)))
+    screen_size = np.array([1000, 1000])
+    game = Game(screen_size)
+    game.space_system.spawn_bodies_on_circle(10, screen_size / 2, 200, 1.5)
     game.run()
